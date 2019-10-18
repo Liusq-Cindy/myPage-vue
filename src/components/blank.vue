72c9df0096b59101625483f4915aeda51001a32d
lsq <306749392@qq.com>
2019-10-18 18:01:33 +0800

    feat: 初始化项目

---
+++ src/components/blank.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container">
+      空白页
+    </div>
+  </div>
+</template>
+
+<script>
+// 调用组件
+export default {
+  name: 'sortable',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less">
+</style>
